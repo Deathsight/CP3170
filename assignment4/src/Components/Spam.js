8ae0c8e4042ec26c83122489ad381c8a489c1be9
Deathsight <57475781+Deathsight@users.noreply.github.com>
2019-11-13 09:20:43 +0300

    Add files via upload

---
+++ assignment4/src/Components/Spam.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+// import "./App.css";
+
+export default class Spam extends React.Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <Typography paragraph>SPAM</Typography>
+      </React.Fragment>
+    );
+  }
+}
